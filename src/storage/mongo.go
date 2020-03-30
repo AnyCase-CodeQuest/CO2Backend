9c6015434a3a064b77e0bfffb7d9016bf73dad94
Mikalai Sahnoiski <sahnovski@gmail.com>
2020-03-31 01:07:54 +0300

    fucking clusters

--- src/storage/mongo.go
+++ src/storage/mongo.go
@@ -2,11 +2,12 @@ package storage
 
 import (
 	"context"
+	"github.com/AnyCase-Company-LTD/CO2Backend/src/values"
+	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	auth2 "go.mongodb.org/mongo-driver/x/mongo/driver/auth"
-	"log"
 	"os"
 )
 
@@ -14,21 +15,25 @@ var Client *mongo.Client
 
 func InitClient() {
 	if isConnectionActive() != true {
-		log.Println("made new connection")
-		clientOptions := options.Client().ApplyURI(os.Getenv("DATABASE_CONNECTION"))
+		log.Debug("made new connection")
+
 		auth := options.Credential{
-			Password:      os.Getenv("PASSWORD"),
-			Username:      os.Getenv("USERNAME"),
+			Password:      os.Getenv(values.DbPassword),
+			Username:      os.Getenv(values.DbUsername),
+			AuthSource:    os.Getenv(values.DbName),
 			AuthMechanism: auth2.SCRAMSHA1,
-			AuthSource:    os.Getenv("USERNAME")}
-		clientOptions.SetAuth(auth)
-		var err error
-		Client, err = mongo.NewClient(clientOptions)
-		if err != nil {
-			log.Fatal(err)
 		}
-		err = Client.Connect(context.TODO())
+		clientOptions := options.Client().ApplyURI(os.Getenv(values.DbConnection)).SetAuth(auth)
+		var err error
+		log.Debug("db connection try to connect")
+		//Client, err = mongo.NewClient(clientOptions)
+		//if err != nil {
+		//	log.Errorf("db connection cant establish: %s", err.Error())
+		//	log.Fatal(err)
+		//}
+		Client, err = mongo.Connect(context.TODO(), clientOptions)
 		if err != nil {
+			log.Errorf("db connection connect error: %e", err.Error())
 			log.Fatal(err)
 		}
 	}
@@ -36,22 +41,25 @@ func InitClient() {
 
 func isConnectionActive() bool {
 	if Client == nil {
+		log.Debug("db connection not exist")
 		return false
 	}
 	err := Client.Ping(context.Background(), readpref.Primary())
 	if err != nil {
+		log.Debug("db connection is die")
 		return false
 	} else {
+		log.Debug("db connection is active")
 		return true
 	}
 }
 
 func GetDataCollection() *mongo.Collection {
 	InitClient()
-	return Client.Database(os.Getenv("DATABASE")).Collection(os.Getenv("COLLECTION_DATA"))
+	return Client.Database(os.Getenv(values.DbName)).Collection(os.Getenv("COLLECTION_DATA"))
 }
 
 func GetSensorCollection() *mongo.Collection {
 	InitClient()
-	return Client.Database(os.Getenv("DATABASE")).Collection(os.Getenv("COLLECTION_SENSOR"))
+	return Client.Database(os.Getenv(values.DbName)).Collection(os.Getenv("COLLECTION_SENSOR"))
 }
--- src/values/constants.go
+++ src/values/constants.go
@@ -6,6 +6,11 @@ const EnvQueueHost string = "QUEUE_SERVER_HOST"
 const EnvQueuePort string = "QUEUE_SERVER_PORT"
 const EnvQueueName string = "QUEUE_NAME"
 
+const DbConnection string = "DATABASE_CONNECTION"
+const DbUsername string = "USERNAME"
+const DbPassword string = "PASSWORD"
+const DbName string = "DATABASE"
+
 const EnvEnv string = "ENV"
 const EnvProd string = "production"
 const EnvDev string = "dev"
